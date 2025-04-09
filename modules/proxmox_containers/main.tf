416d62b4af340d00fcbf1a145d138660df719599
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2025-04-09 10:44:56 +0200

    fix

--- modules/proxmox_containers/main.tf
+++ modules/proxmox_containers/main.tf
@@ -1,11 +1,6 @@
 terraform {
   required_version = ">=1.11.0"
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">=2.5.2"
-    }
-
     null = {
       source  = "hashicorp/null"
       version = ">=3.2.3"
--- providers.tf
+++ providers.tf
@@ -13,11 +13,6 @@ terraform {
       version = ">=2.31.0"
     }
 
-    local = {
-      source  = "hashicorp/local"
-      version = ">=2.5.2"
-    }
-
     proxmox = {
       source  = "bpg/proxmox"
       version = ">=0.75.0"
